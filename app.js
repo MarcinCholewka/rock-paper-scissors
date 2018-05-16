1e0c16f6222f90a06385e736a3396e0114f724f2
Marcin Cholewka <marcin@marcincholewka.pl>
2018-05-16 20:54:03 +0200

    Add files html, css js and folder images

---
+++ app.js
@@ -0,0 +1,108 @@
+var name;
+var rounds;
+var user_div = document.getElementById('user-label');
+var button = document.getElementById('greeter-button');
+var output = document.getElementById('greeter-output');
+var userScore = 0;
+var computerScore = 0;
+var userScore_span = document.getElementById('user-score');
+var computerScore_span = document.getElementById('computer-score');
+var scoreBoard_div = document.querySelector('#output');
+var result_p = document.querySelector('#result > p');
+var rock_div = document.getElementById('Rock');
+var paper_div = document.getElementById('Paper');
+var scissors_div = document.getElementById('Scissors');
+var games_p = document.getElementById('games-message');
+
+
+function computerMove() {
+    var choices = ['Rock', 'Paper', 'Scissors'];
+    var randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function win(userChoice, computerChoice) {
+    userScore++
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = 'YOU WON : you played ' + userChoice + ', computer played ' + computerChoice;
+    document.getElementById(userChoice).classList.remove('red-glow');
+    document.getElementById(userChoice).classList.remove('yellow-glow');
+    document.getElementById(userChoice).classList.add('green-glow');
+    if (userScore >= rounds) {
+        alert(name + '. You won the entire game ' + userScore + ':' + computerScore + '.');
+        alert('GAME OVER!!! Press "Click to play" to play again.');
+    }
+}
+
+function lose(userChoice, computerChoice) {
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = 'YOU LOST : you played ' + userChoice + ', computer played ' + computerChoice;
+    document.getElementById(userChoice).classList.remove('green-glow');
+    document.getElementById(userChoice).classList.remove('yellow-glow');
+    document.getElementById(userChoice).classList.add('red-glow');
+    if (computerScore >= rounds) {
+        alert(name + '. You lost the entire game ' + userScore + ':' + computerScore + '.');
+        alert('GAME OVER!!! Press "Click to play" to play again.');
+    }
+};
+
+
+function draw(userChoice, computerChoice) {
+    result_p.innerHTML = 'ITS A DRAW : you played ' + userChoice + ', computer played ' + computerChoice;
+    document.getElementById(userChoice).classList.remove('green-glow');
+    document.getElementById(userChoice).classList.remove('red-glow');
+    document.getElementById(userChoice).classList.add('yellow-glow');
+};
+
+function playerMove(userChoice) {
+    var computerChoice = computerMove();
+    if (userChoice === 'Rock' && computerChoice === 'Scissors') {
+        win(userChoice, computerChoice);
+    } else if (userChoice === 'Paper' && computerChoice === 'Rock') {
+        win(userChoice, computerChoice);
+    } else if (userChoice === 'Scissors' && computerChoice === 'Paper') {
+        win(userChoice, computerChoice);
+    } else if (userChoice === 'Rock' && computerChoice === 'Paper') {
+        lose(userChoice, computerChoice);
+    } else if (userChoice === 'Paper' && computerChoice === 'Scissors') {
+        lose(userChoice, computerChoice);
+    } else if (userChoice === 'Scissors' && computerChoice === 'Rock') {
+        lose(userChoice, computerChoice);
+    } else if (userChoice === 'r' && computerChoice === 'r') {
+        draw(userChoice, computerChoice);
+    } else if (userChoice === 'p' && computerChoice === 'p') {
+        draw(userChoice, computerChoice);
+    } else if (userChoice === 's' && computerChoice === 's') {
+        draw(userChoice, computerChoice);
+    }
+};
+
+function main() {
+    rock_div.addEventListener('click', function() {
+        playerMove('Rock');      
+    })
+    paper_div.addEventListener('click', function() {
+        playerMove('Paper');
+    })
+    scissors_div.addEventListener('click', function() {
+        playerMove('Scissors');
+    })
+};
+
+
+button.addEventListener('click', function(){
+    userScore = 0;
+    userScore_span.innerHTML = userScore;
+    computerScore = 0;
+    computerScore_span.innerHTML = computerScore;
+    name = window.prompt('Hello. You want to play ??? What is your name?');
+    user_div.innerHTML = name;
+    games = window.prompt('Hello ' + name + '. How many rounds you want to play to determine the winner ?');
+    games_p.innerHTML = 'Win ' + games + ' rounds, to win the entire game.';
+    rounds = parseInt(games);
+});
+
+main();
